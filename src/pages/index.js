2afa7e86f9fb739644fc5cdc91d4e65c5a88016c
HerrTxbias <admin@herrtxbias.net>
2021-05-28 17:45:39 +0200

    fix broken links

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/main/intro">
             Utilbot Introduction
           </Link>
         </div>
